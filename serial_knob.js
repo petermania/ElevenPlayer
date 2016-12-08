4b4bd142266192a3c13421a528c2bf4130112cf7
lpercifield@gmail.com <lpercifield@gmail.com>
2016-12-08 15:20:57 -0500

    better serial

--- serial_knob.js
+++ serial_knob.js
@@ -1,5 +1,6 @@
 var SerialPort = require('serialport');
 var dataCallback;
+var dataBuf = "";
 SerialPort.list(function (err, ports) {
   ports.forEach(function(port) {
     console.log(port.comName);
@@ -20,10 +21,23 @@ port.on('open', function() {
   console.log("Port Open");
 });
 port.on('data', function (data) {
-  var dataInt = parseInt(data);
-  dataCallback(dataInt);
-});
+  dataBuf += data.toString();
+  //console.log(dataBuf);
+  if (dataBuf.indexOf('\n') > 0) {
+  //if (dataBuf.indexOf('\n') > 0 && dataBuf.indexOf('*') > 0) {
+    dataBuf = dataBuf.trim();
+    var dataInt = parseInt(dataBuf.slice(1));
+    //console.log(dataBuf);
+    dataBuf = '';
+    if(!isNaN(dataInt)){
+      dataCallback(dataInt);
+    }
+  }
 
+});
+// setup(function(data){
+//   console.log(data);
+// })
 module.exports = {
   setup:setup
 }
