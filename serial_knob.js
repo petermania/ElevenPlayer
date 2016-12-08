9fbf6efe8ef20c331339ef5a0e1b370a6b78a354
lpercifield@gmail.com <lpercifield@gmail.com>
2016-12-08 14:16:39 -0500

    add serial_knob callback

--- serial_knob.js
+++ serial_knob.js
@@ -7,25 +7,21 @@ SerialPort.list(function (err, ports) {
     console.log(port.manufacturer);
   });
 });
+var port = new SerialPort('/dev/cu.usbmodem31', {baudRate:115200, autoOpen:false});
 function setup(callback){
+  port.open(function (err) {
+  if (err) {
+    return console.log('Error opening port: ', err.message);
+  }
+});
   dataCallback = callback;
 }
-var port = new SerialPort('/dev/cu.usbmodem31', {baudRate:115200
-  //parser: SerialPort.parsers.readline('\n')
-});
-// var port = new SerialPort('/dev/cu.usbmodem31', function (err) {
-//   if (err) {
-//     return console.log('Error: ', err.message);
-//   }
-// });
 port.on('open', function() {
   console.log("Port Open");
 });
 port.on('data', function (data) {
-  //console.log(data.length);
   var dataInt = parseInt(data);
   dataCallback(dataInt);
-  //console.log(parseInt(data));
 });
 
 module.exports = {
