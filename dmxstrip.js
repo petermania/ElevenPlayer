3aa7d51d81b228a102f346a2cf4f21a615866317
lpercifield@gmail.com <lpercifield@gmail.com>
2016-12-08 12:30:11 -0500

    add zer

--- dmxstrip.js
+++ dmxstrip.js
@@ -707,7 +707,10 @@ function activateNumber(number, callback) {
             merge(activeObj, setBlock(setupObj.number[i].numValue, Color(setupObj.number[i].standbyColor)))
         } else if (setupObj.number[i].numValue == number) {
             merge(activeObj, setBlock(setupObj.number[i].numValue, Color(setupObj.number[i].activeColor)))
-        } else {
+        }else if (number == 0){
+            merge(activeObj, setBlock(setupObj.number[i].numValue, offColor))
+        }
+        else {
             merge(activeObj, setBlock(setupObj.number[i].numValue, offColor))
         }
     }
